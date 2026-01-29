3562af120f0035acdd2d7483c648768578c83e90
Banan <Dazzen108@yandex.ru>
2026-01-29 23:38:15 +0300

    шаблонная функция группировки

--- lab9.cpp
+++ lab9.cpp
@@ -13,3 +13,56 @@ struct GroupInfo {
     double sum;       // сумма valueFunc(x) по группе
     double avg;       // среднее sum / count (вещественное)
 };
+// шаблонная функция группировки
+template <typename T, typename KeyFunc, typename ValueFunc>
+auto groupStats(T* arr, int n, KeyFunc keyFunc, ValueFunc valueFunc)
+{
+    // тип ключа берём через decltype
+    using KeyType = decltype(keyFunc(std::declval<T>()));
+
+    std::vector<GroupInfo<KeyType, T>> groups;
+
+    for (int i = 0; i < n; ++i) {
+        T x = arr[i];
+        KeyType key = keyFunc(x);
+        double value = (double)valueFunc(x);
+
+        // поиск группы по ключу 
+        int index = -1;
+        for (int j = 0; j < (int)groups.size(); ++j) {
+            if (groups[j].key == key) {
+                index = j;
+                break;
+            }
+        }
+
+        // если группы нет то создаём
+        if (index == -1) {
+            GroupInfo<KeyType, T> g;
+            g.key = key;
+            g.count = 1;
+            g.minElem = x;
+            g.maxElem = x;
+            g.sum = value;
+            g.avg = value; 
+
+            groups.push_back(g);
+        }
+        // если группа есть то обновляем 
+        else {
+            groups[index].count++;
+
+            // min/max 
+            if (x < groups[index].minElem)
+                groups[index].minElem = x;
+
+            if (groups[index].maxElem < x)
+                groups[index].maxElem = x;
+
+            groups[index].sum += value;
+            groups[index].avg = groups[index].sum / groups[index].count;
+        }
+    }
+
+    return groups;
+}
