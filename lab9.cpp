29e37f005559a217915f6dbea4f18c965297ed65
Banan <Dazzen108@yandex.ru>
2026-01-30 00:36:47 +0300

    Update lab9.cpp main

--- lab9.cpp
+++ lab9.cpp
@@ -81,3 +81,63 @@ void printGroups(const std::vector<GroupInfo<KeyType, ElemType>>& groups)
                   << g.avg << "\n";
     }
 }
+
+int main()
+{
+    // 1) Массив int
+    // ключ: последняя цифра x % 10
+    // value: само число x
+    int a[] = {12, 25, 30, 41, 52, 63, 70, 81, 92, 103};
+    int n1 = sizeof(a) / sizeof(a[0]);
+
+    auto groupsInt = groupStats(
+        a, n1,
+        [](int x) { return x % 10; },
+        [](int x) { return x; }
+    );
+
+    std::cout << "\nINT ARRAY\n";
+    printGroups(groupsInt);
+
+    // 2) Массив double 
+    // ключ: номер интервала (0: x<0, 1: 0<=x<=10, 2: x>10)
+    // value: само число x
+    double b[] = {-2.5, 0.0, 3.1, 10.0, 10.2, 25.7, -0.1, 7.7};
+    int n2 = sizeof(b) / sizeof(b[0]);
+
+    auto groupsDouble = groupStats(
+        b, n2,
+        [](double x) {
+            if (x < 0) return 0;
+            if (0 <= x && x <= 10) return 1;
+            return 2;
+        },
+        [](double x) { return x; }
+    );
+
+    std::cout << "\n double array \n";
+    printGroups(groupsDouble);
+
+    // 3) Массив std::string 
+    // ключ: первая буква строки (char)
+    // value: длина строки
+    std::string s[] = {"apple", "apricot", "banana", "blue", "cherry", "car", ""};
+    int n3 = sizeof(s) / sizeof(s[0]);
+
+    auto groupsString = groupStats(
+        s, n3,
+        [](const std::string& x) {
+            // на случай пустой строки
+            if (x.empty()) return '?';
+            return x[0];
+        },
+        [](const std::string& x) {
+            return (double)x.length();
+        }
+    );
+
+    std::cout << "\n string array\n";
+printGroups(groupsString);
+
+    return 0;
+}
